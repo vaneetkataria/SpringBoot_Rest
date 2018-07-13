78bf85051587d901649f398102cf1c380d9d3a71
vaneet <vaneetkataria54@gmail.com>
2018-07-13 13:35:48 +0530

    Configured Exception Handlers

--- Restful/src/main/java/com/kataria/springboot/rest/practice/web/common/execption/handler/DefaultExceptionHandler.java
+++ Restful/src/main/java/com/kataria/springboot/rest/practice/web/common/execption/handler/DefaultExceptionHandler.java
@@ -2,28 +2,24 @@ package com.kataria.springboot.rest.practice.web.common.execption.handler;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.core.Ordered;
-
 import com.kataria.springboot.rest.practice.core.beans.RestResponse;
 import com.kataria.springboot.rest.practice.core.exception.CoreException;
 
 @ControllerAdvice()
 @RestController
-@Order(Ordered.HIGHEST_PRECEDENCE)
 public class DefaultExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(CoreException.class)
@@ -37,16 +33,18 @@ public class DefaultExceptionHandler extends ResponseEntityExceptionHandler {
 		return finalResponse;
 	}
 
-	@ExceptionHandler(Exception.class)
-	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-	public RestResponse handleAllException(Exception e, HttpServletRequest request) {
+	// Any exception handled by ResponseEntityExceptionHandler will now be dealt
+	// like below defined.
+	@Override
+	public ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
 		RestResponse restResponse = new RestResponse();
-		String message = e.getMessage();
-		restResponse.setMessage(StringUtils.hasLength(message) ? message : HttpStatus.INTERNAL_SERVER_ERROR.name())
-				.setPath(request.getRequestURI());
-		return restResponse;
+		restResponse.setMessage(ex.getMessage()).setPath(request.getDescription(false));
+		return new ResponseEntity<>(restResponse, headers, status);
 	}
 
+	// Overriding this method specially as separate handling for extracting text
+	// defined in Validation annotations is required.
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
