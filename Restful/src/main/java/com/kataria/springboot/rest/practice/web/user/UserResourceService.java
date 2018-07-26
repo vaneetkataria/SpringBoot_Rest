a50e45e6393b6a3a7fb4fe975893d86894748bf4
vaneet <vaneetkataria54@gmail.com>
2018-07-26 23:21:50 +0530

    optimised imports

--- Restful/src/main/java/com/kataria/springboot/rest/practice/web/user/UserResourceService.java
+++ Restful/src/main/java/com/kataria/springboot/rest/practice/web/user/UserResourceService.java
@@ -7,8 +7,6 @@ import javax.validation.constraints.Positive;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
